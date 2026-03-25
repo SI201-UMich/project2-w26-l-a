929a0785d0d721ffbdd0769950fa5ec4aa5e8202
Ailyn Moreno <ailynm@umich.edu>
2026-03-25 01:39:44 -0400

    completed validate_policy_numbers

--- project2_starter.py
+++ project2_starter.py
@@ -284,7 +284,16 @@ def validate_policy_numbers(data) -> list[str]:
     # ==============================
     # YOUR CODE STARTS HERE
     # ==============================
-    pass
+    misformatted_listings = []
+    for row in data:
+        listing_id = row[1]
+        policy_number = row[2]
+        if policy_number in ["Pending", "Exempt"]:
+            continue
+        if not re.match(r"^(20\d{2}-00\d{4}STR|STR-000\d{4})$", policy_number):
+            misformatted_listings.append(listing_id)
+    return misformatted_listings
+    #pass
     # ==============================
     # YOUR CODE ENDS HERE
     # ==============================
